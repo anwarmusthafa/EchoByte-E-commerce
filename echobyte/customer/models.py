87a6f61bd934ca208ac995fed974aa543588cad6
unknown <anwarmusthafa99@gmail.com>
2024-01-18 18:57:27 +0530

    feat:- User registraion added

--- echobyte/customer/models.py
+++ echobyte/customer/models.py
@@ -1,8 +1,23 @@
-# accounts/models.py
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import User
 
-# class CustomUser(AbstractUser):
-#     phone_number = models.CharField(max_length=15, blank=True)
- 
+class Customer(models.Model):
+    LIVE = 1
+    DELETE = 0
+    DELETE_CHOICES = ((LIVE, 'Live'), (DELETE, 'Delete'))
+    phone = models.CharField(max_length=50, unique=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    delete_status = models.IntegerField(choices=DELETE_CHOICES, default=LIVE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses', null = True)  # Added related_name
+    name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    address = models.TextField()
+    pincode = models.PositiveIntegerField()
+    landmark = models.CharField(max_length=50, blank=True, null=True)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
--- echobyte/customer/views.py
+++ echobyte/customer/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from .models import Customer
 
-# Create your views here.
 def signin(request):
+    
     return render(request, 'signin.html')
+
 def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')  # Fix: Corrected the key to 'password2'
+        phone = request.POST.get('phone')
+
+        if password1 == password2:
+            # Fix: Use 'create_user' instead of 'create'
+            user = User.objects.create(username=username, email=email, password=password1)
+            # Assuming you want to log the user in immediately after signup
+            customer = Customer.objects.create(user = user, phone = phone)
+            return redirect('home')  # Change 'home' to your desired redirect path
+        else:
+            # Handle password mismatch error
+            return render(request, 'signup.html', {'error_message': 'Passwords do not match'})
+
     return render(request, 'signup.html')

c525916e5cf9a74c8c9ab0243b2f9f2c042ae9a7
unknown <anwarmusthafa99@gmail.com>
2024-01-29 11:20:44 +0530

    refactor :- Product related functions are shifted to product app from admin app

--- echobyte/app_admin/urls.py
+++ echobyte/app_admin/urls.py
@@ -7,12 +7,4 @@ urlpatterns = [
     path('admin_logout/', views.admin_logout, name= "admin_logout"),
     path('customers_list/', views.customers_list, name= "customers_list"),
     path('delete_status/<pk>/', views.delete_status, name='delete_status'),
-    path('list_product/', views.list_product, name='list_product'),
-    path('product_delete/<pk>/', views.product_delete, name='product_delete'),
-    path('add_product/', views.add_product, name='add_product'),
-    path('add_variant/', views.add_variant, name='add_variant'),
-    path('list_variants/', views.list_variants, name='list_variants'),
-    path('variant_block/<pk>/', views.variant_block, name='variant_block'),
-    path('edit_product/<pk>/', views.edit_product, name='edit_product'),
-    path('edit_variant/<pk>/', views.edit_variant, name='edit_variant'),
-] 
+]
--- echobyte/product/urls.py
+++ echobyte/product/urls.py
@@ -4,4 +4,13 @@ from . import views
 urlpatterns = [
     path('all_products/', views.all_products, name= "all_products"),
     path('product_detail/<pk>/', views.product_detail, name = "product_detail"),
-]
+    path('list_product/', views.list_product, name='list_product'),
+    path('product_delete/<pk>/', views.product_delete, name='product_delete'),
+    path('add_product/', views.add_product, name='add_product'),
+    path('add_variant/', views.add_variant, name='add_variant'),
+    path('list_variants/', views.list_variants, name='list_variants'),
+    path('variant_block/<pk>/', views.variant_block, name='variant_block'),
+    path('edit_product/<pk>/', views.edit_product, name='edit_product'),
+    path('edit_variant/<pk>/', views.edit_variant, name='edit_variant'),
+] 
+

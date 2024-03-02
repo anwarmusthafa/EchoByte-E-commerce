1d2f257283f3b94a118e6d16ebde7872f328aef3
unknown <anwarmusthafa99@gmail.com>
2024-03-02 13:16:15 +0530

    feat: latest arrivals shown in the home page

--- echobyte/user_home/views.py
+++ echobyte/user_home/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
 from django.views.decorators.cache import never_cache
 from django.contrib.auth.decorators import user_passes_test
+from product.models import ProductVariant
+from django.db.models import Q
 
 # Create your views here.
 @never_cache
 
 def home(request):
-    return render(request, 'index.html')
+    new_arrivals = ProductVariant.objects.exclude(
+    Q(product__delete_status=0) |
+    Q(product__is_listed=False) |
+    Q(product__category__is_listed=False) |
+    Q(is_listed=False)).order_by('product__title', '-product__created_at').distinct('product__title')[:6]
+
+    context = {'new_arrivals':new_arrivals}
+    return render(request, 'index.html',context)
 def about(request):
     return render(request, 'about.html')
 def contact(request):
     return render(request, 'contact.html')
+    
+        
 
 
 
